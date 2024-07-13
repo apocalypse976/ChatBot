9422718534299655128de555020d6e86e793f586
apocalypse976 <aarunichatterjee@gmail.com>
2024-07-13 21:53:33 +0530

    2

--- Env/app.py
+++ Env/app.py
@@ -1,1 +1,11 @@
 import nltk
+nltk.download('punkt')
+def tokenise(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(words):
+    pass
+
+
+def bag_of_words(tokenised_sentence, all_words):
+    pass
